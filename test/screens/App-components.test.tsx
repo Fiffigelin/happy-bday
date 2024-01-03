112e8eb38d5d42ca17f312b7447b14028ed0d653
Elin Nyman <elin.nyman87@gmail.com>
2024-01-03 18:31:33 +0100

    Removed outdated tests

--- test/screens/App-components.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const MockBirthdaysMessagesStackNavigator = () => (
-  <div data-testid="mock-BirthdaysMessagesStackNavigator" />
-);
-export const MockContactsStackNavigator = () => (
-  <div data-testid="mock-ContactsStackNavigator" />
-);
--- test/screens/App.test.tsx
+++ test/screens/App.test.tsx
@@ -23,34 +23,3 @@
 //     expect(appContainer.props.style.backgroundColor).toBe("#fff");
 //   });
 // });
-
-import TabNavigator from "@/src/navigation/TabNavigator";
-import * as ReactNavigation from "@react-navigation/native";
-import { render } from "@testing-library/react-native";
-import {
-  MockBirthdaysMessagesStackNavigator,
-  MockContactsStackNavigator,
-} from "./App-components.test";
-
-jest.mock("@/src/navigation/BirthdayMessagesNavigator", () => ({
-  __esModule: true,
-  default: jest.fn().mockReturnValue(<MockBirthdaysMessagesStackNavigator />),
-}));
-
-jest.mock("@/src/navigation/ContactsNavigator", () => ({
-  __esModule: true,
-  default: jest.fn().mockReturnValue(<MockContactsStackNavigator />),
-}));
-
-describe("TabNavigator", () => {
-  test("renders correct screens inside Tab.Navigator", () => {
-    const mockNavigation = jest.spyOn(ReactNavigation, "useNavigation");
-
-    render(<TabNavigator />);
-
-    expect(mockNavigation).toHaveBeenCalledWith(
-      "mock-BirthdaysMessagesStackNavigator"
-    );
-    expect(mockNavigation).toHaveBeenCalledWith("mock-ContactsStackNavigator");
-  });
-});
