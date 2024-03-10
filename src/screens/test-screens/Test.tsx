5740fe722eb150175a36423bb3a141888d229ff9
Elin Nyman <elin.nyman87@gmail.com>
2024-03-10 16:10:09 +0100

    List birthdays images

--- src/screens/test-screens/Test.tsx
+++ src/screens/test-screens/Test.tsx
@@ -1,29 +1,15 @@
+import { Category, Image as Images } from "@/src/api/image/image.api";
+import CustomImageCarousel from "@/src/components/customImageCarousel";
 import { fetchImagesAPI } from "@/src/features/image/image.slice";
 import { useAppDispatch, useAppSelector } from "@/src/features/store";
 import SortImagesService from "@/src/services/sortImages.service";
 import React, { useEffect, useState } from "react";
-import { Image, StyleSheet, View } from "react-native";
-import PagerView from "react-native-pager-view";
-import { Category, Image as Images } from "../../api/image/image.api";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Test() {
-  const [categoryPeople, setCategoryPeople] = useState<Images[]>();
-  const [categoryAnimals, setCategoryAnimals] = useState<Images[]>();
-  const [categoryDinos, setCategoryDinos] = useState<Images[]>();
+  const [imageArray, setImageArray] = useState<(Images[] | undefined)[]>([]);
   const dispatch = useAppDispatch();
   const images = useAppSelector((state) => state.image.images);
-
-  function ImageCarouselComponent({ images }: { images?: Images[] }) {
-    return (
-      <PagerView style={styles.viewPager} initialPage={0}>
-        {images?.map((image, index) => (
-          <View style={styles.page} key={index.toString()}>
-            <Image source={{ uri: image.url }} style={styles.image} />
-          </View>
-        ))}
-      </PagerView>
-    );
-  }
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,73 +21,31 @@ export default function Test() {
 
   useEffect(() => {
     const sortedImages = SortImagesService.sortImagesByCategory(images!);
-    setCategoryPeople(sortedImages[Category.People]);
-    setCategoryAnimals(sortedImages[Category.Animals]);
-    setCategoryDinos(sortedImages[Category.Dinos]);
+
+    setImageArray([
+      sortedImages[Category.People],
+      sortedImages[Category.Animals],
+      sortedImages[Category.Dinos],
+    ]);
   }, [images]);
 
-  useEffect(() => {
-    console.log("Category People: ", categoryPeople);
-  }, [categoryPeople]);
-
-  useEffect(() => {
-    console.log("Category Animals: ", categoryAnimals);
-  }, [categoryAnimals]);
-
-  useEffect(() => {
-    console.log("Category Dinos: ", categoryDinos);
-  }, [categoryDinos]);
   return (
     <View style={{ flex: 1 }}>
-      <ImageCarouselComponent images={categoryPeople} />
+      <ScrollView>
+        {imageArray?.map((category) => (
+          <View>
+            <Text></Text>
+            <CustomImageCarousel images={category} />
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 50,
-  },
-  tinyLogo: {
-    width: 200,
-    height: 200,
-  },
-  viewPager: {
     flex: 1,
-  },
-  page: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  image: {
-    width: 200,
-    height: 200,
-    resizeMode: "cover",
+    backgroundColor: "white",
   },
 });
-
-// useEffect(() => {
-//   const fetchData = async () => {
-//     await dispatch(fetchImagesAPI());
-//     console.log("IMAGES: ", images);
-//     const sortedImages = SortImagesService.sortImagesByCategory(images!);
-//     setCategoryPeople(sortedImages[Category.People]);
-//     console.log("Category People: ", categoryPeople);
-//     setCategoryAnimals(sortedImages[Category.Animals]);
-//     console.log("Category Animals: ", categoryAnimals);
-//     setCategoryDinos(sortedImages[Category.Dinos]);
-//     console.log("Category Dinos: ", categoryDinos);
-//   };
-
-//   fetchData();
-// }, []);
-
-// return (
-//   <View style={styles.container}>
-//     <Image
-//       source={{ uri: "https://i.imgur.com/d6GTsjo.jpg" }}
-//       style={styles.tinyLogo}
-//     />
-//     <Text>Lalalala</Text>
-//   </View>
-// );
