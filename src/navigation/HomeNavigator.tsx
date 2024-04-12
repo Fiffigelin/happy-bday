ccf4f20113f38a8e3ce8a1bc17a2a6ea51fe81c3
Elin Nyman <elin.nyman87@gmail.com>
2024-04-12 14:29:09 +0200

    Implemented custom header to stack navigator

--- src/navigation/HomeNavigator.tsx
+++ src/navigation/HomeNavigator.tsx
@@ -1,6 +1,7 @@
 import { HomeStackParamList } from "@/src/navigation/NavigationTypes";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+import CustomStackHeader from "../components/stack-header/customStackHeader";
 import CreateMessage from "../screens/home/CreateMessage";
 import HandleMessage from "../screens/home/HandleMessage";
 import HomeScreen from "../screens/home/HomeScreen";
@@ -9,7 +10,11 @@ const Stack = createNativeStackNavigator<HomeStackParamList>();
 
 export default function HomeStackNavigator() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        header: (props) => <CustomStackHeader />,
+      }}
+    >
       <Stack.Screen
         name="Home"
         component={HomeScreen}
